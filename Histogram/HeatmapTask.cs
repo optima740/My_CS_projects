4b9388afb6cea435d48bf551a320653c03433b06
Andrey Melnikov <k253vo@mail.ru>
2021-03-28 17:17:18 +0300

    28-03-21

---
+++ Histogram/HeatmapTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var birthData = new string[30];
+            for (int i = 0; i < 30; i++) birthData[i] = (i + 2).ToString();
+            var numderMonth = new string[12];
+            for (int i = 0; i < 12; i++) numderMonth[i] = (i + 1).ToString();
+            var countEquals = new double[30, 12];         
+            foreach (var item in names)
+            {
+                var tempDay = (int)item.BirthDate.Day;
+                var tempMonth = (int)item.BirthDate.Month;
+                if (tempDay != 1) 
+                    countEquals[tempDay - 2, tempMonth - 1]++;
+            }           
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                countEquals,
+                birthData,
+                numderMonth);
+        }
+    }
+}
---
+++ Histogram/HistogramTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var birthData = new string[31];
+            birthData[0] = "1";
+            for (int i = 1; i < 31; i++)
+            {
+                birthData[i] = (i+1).ToString();
+            }          
+            var countEquals = new double[31];
+            var index = 1;           
+            for (int i = 1; i < 31; i++)
+            {               
+                var date = birthData[i];
+                //if (i < 9) date = "0" + birthData[i];               
+                foreach (var item in names)
+                {
+                    var tempDay = item.BirthDate.Day.ToString();
+                    var tempName = item.Name;
+                    if (tempName.Equals(name, StringComparison.OrdinalIgnoreCase) && tempDay.Equals(date, StringComparison.OrdinalIgnoreCase)) countEquals[index]++;
+                }              
+                index++;               
+            }
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                birthData, 
+                countEquals);
+        }
+    }
+}
